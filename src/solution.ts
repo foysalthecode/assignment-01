7b74ee257f17fa24bb1d2a47c75c9a184cd2e52d
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-18 03:09:03 +0600

    solve the third problem

--- src/solution.ts
+++ src/solution.ts
@@ -20,3 +20,15 @@ const getLength = (value: StringOrArr) => {
     return value.length;
   }
 };
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    (this.name = name), (this.age = age);
+  }
+
+  getDetails() {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
